ec2ae8cfdac358187410b373efced86c44a58533
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-09-28 17:34:31 +0900

    initial commit

---
+++ src/main/java/sogang/capstone/editking/EditkingApplication.java
@@ -0,0 +1,13 @@
+package sogang.capstone.editking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sogang/capstone/editking/EditkingApplicationTests.java
@@ -0,0 +1,13 @@
+package sogang.capstone.editking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EditkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
