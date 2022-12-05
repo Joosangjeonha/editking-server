ecd0d33b2ac098dd6a964f87c4ec7f11cb6e390b
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-12-05 17:11:58 +0900

    del: 미사용 DTO

--- src/main/java/sogang/capstone/editking/domain/user/auth/CustomUserDetailServiceImpl.java
+++ src/main/java/sogang/capstone/editking/domain/user/auth/CustomUserDetailServiceImpl.java
@@ -1,15 +1,11 @@
 package sogang.capstone.editking.domain.user.auth;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.ToString;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import sogang.capstone.editking.domain.user.NoUserException;
-import sogang.capstone.editking.domain.user.User;
 import sogang.capstone.editking.domain.user.UserReader;
 
 @RequiredArgsConstructor
@@ -22,19 +18,5 @@ public class CustomUserDetailServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return (UserDetails) userReader.findById(Long.parseLong(username))
                 .orElseThrow(() -> new NoUserException());
-    }
-
-    @Getter
-    @ToString
-    @EqualsAndHashCode
-    public static class TokenDTO {
-
-        private Long id;
-        private String name;
-
-        public TokenDTO(User user) {
-            this.id = user.getId();
-            this.name = user.getName();
-        }
     }
 }
