29adc9569c27f4822c550e40f8b0e92406cca3e6
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-12-10 16:21:00 +0900

    feat: 비동기 처리 #46

--- src/main/java/sogang/capstone/editking/EditkingApplication.java
+++ src/main/java/sogang/capstone/editking/EditkingApplication.java
@@ -2,12 +2,14 @@ package sogang.capstone.editking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class EditkingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EditkingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EditkingApplication.class, args);
+    }
 
 }
--- src/main/java/sogang/capstone/editking/domain/form/event/SubmittedEventHandler.java
+++ src/main/java/sogang/capstone/editking/domain/form/event/SubmittedEventHandler.java
@@ -2,6 +2,7 @@ package sogang.capstone.editking.domain.form.event;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import sogang.capstone.editking.domain.interview.InterviewService;
 
@@ -11,6 +12,7 @@ public class SubmittedEventHandler implements ApplicationListener<SubmittedEvent
 
     private final InterviewService interviewService;
 
+    @Async
     @Override
     public void onApplicationEvent(SubmittedEvent event) {
         interviewService.analyzeInterview(event.getForm());
